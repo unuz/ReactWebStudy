97d9b6952ec7ef38c044b99bd5afe767fa1e585f
unuz <unuz94@gmail.com>
2020-07-24 12:16:37 +0900

    class component => function component

--- src/routes/Try.jsx
+++ src/routes/Try.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+// class Try extends Component {
+//     render() {
+//         return (
+//             <li>
+//                 <b>{this.props.value.fruit}</b> : {this.props.value.taste}
+//             </li>
+//         );
+//     }
+// }
+import React from 'react';
 
-
-class Try extends Component {
-    render() {
-        return (
-            <li>
-                <b>{this.props.value.fruit}</b> : {this.props.value.taste}
-            </li>
-        );
-    }
-
-}
-
-// const Try = () => {
-//     return (
-//         // key는 고유값을 적어주어야 함
-//         // key를 기준으로 엘리먼트 추가,수정,삭제 판단하기 때문에 index값은 추천하지 않음
-//         <li key={this.props.value.fruit + this.props.value.taste} >
-//             <b>{this.props.value.fruit}</b> : {this.props.value.taste}
-//         </li>
-//     );
-// };
+const Try = (props) => {
+    return (
+        // key는 고유값을 적어주어야 함
+        // key를 기준으로 엘리먼트 추가,수정,삭제 판단하기 때문에 index값은 추천하지 않음
+        <li key={props.value.fruit + props.value.taste} >
+            <b>{props.value.fruit}</b> : {props.value.taste}
+        </li>
+    );
+};
 
 export default Try;
